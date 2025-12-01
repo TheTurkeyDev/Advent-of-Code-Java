f00cf6dc90957696ba9228d8a73703aed203b689
TheTurkeyDev <turkey@theturkey.dev>
2025-12-01 01:53:03 -0500

    2025 Day 1 improved?

--- src/main/java/dev/theturkey/aoc/y2025/Year2025Day01.java
+++ src/main/java/dev/theturkey/aoc/y2025/Year2025Day01.java
@@ -20,28 +20,30 @@ public class Year2025Day01 extends AOCPuzzle
 
 		for(String s : input)
 		{
-			char dir = s.charAt(0);
 			int amount = Integer.parseInt(s.substring(1));
+			part2 += (amount / 100);
 
-			if(dir == 'R')
+			if(s.charAt(0) == 'R')
 			{
-				dialNum = dialNum + amount;
-				if(dialNum >= 100)
+				dialNum = dialNum + (amount % 100);
+				if(dialNum > 99)
 				{
-					part2 += (dialNum / 100);
-					dialNum %= 100;
+					part2 += 1;
+					dialNum -= 100;
 				}
 			}
 			else
 			{
-				boolean atO = dialNum == 0;
-				dialNum = dialNum - amount;
-				if(dialNum <= 0)
+				boolean at0 = dialNum == 0;
+				dialNum = dialNum - (amount % 100);
+				if(dialNum < 0)
 				{
-					part2 += Math.abs(dialNum / 100) + (atO ? 0: 1);
-					dialNum %= 100;
-					if(dialNum < 0)
-						dialNum += 100;
+					part2 += at0 ? 0 : 1;
+					dialNum += 100;
+				}
+				else if(dialNum == 0)
+				{
+					part2 += 1;
 				}
 			}
 

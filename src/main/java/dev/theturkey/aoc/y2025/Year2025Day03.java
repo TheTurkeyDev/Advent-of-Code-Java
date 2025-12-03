101270f9fe07fa71da0c44792b571c326136a94c
TheTurkeyDev <turkey@theturkey.dev>
2025-12-03 00:18:38 -0500

    2025 Day 3

---
+++ src/main/java/dev/theturkey/aoc/y2025/Year2025Day03.java
@@ -0,0 +1,51 @@
+package dev.theturkey.aoc.y2025;
+
+import dev.theturkey.aoc.AOCPuzzle;
+
+import java.util.List;
+
+public class Year2025Day03 extends AOCPuzzle
+{
+	public Year2025Day03()
+	{
+		super("2025", "3");
+	}
+
+	@Override
+	public void solve(List<String> input)
+	{
+		long part1 = 0;
+		for(String s : input)
+			part1 += getJoltage(s, 2);
+		lap(part1);
+
+		long part2 = 0;
+		for(String s : input)
+			part2 += getJoltage(s, 12);
+		lap(part2);
+	}
+
+	private long getJoltage(String line, int numBatteries)
+	{
+		if(line.length() == numBatteries)
+			return Long.parseLong(line);
+
+		int largestIndex = 0;
+		char largest = '0';
+		for(int i = 0; i < line.length() - (numBatteries - 1); i++)
+		{
+			char c = line.charAt(i);
+			if(largest < c)
+			{
+				largest = c;
+				largestIndex = i;
+			}
+		}
+
+		String lineRemain = line.substring(largestIndex + 1);
+		String joltageStr = String.valueOf(largest);
+		if(numBatteries > 1)
+			joltageStr += getJoltage(lineRemain, numBatteries - 1);
+		return Long.parseLong(joltageStr);
+	}
+}
